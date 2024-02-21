de7e21e3ed7d8a8384657d9407cebb06c8b6341e
fredriknystrom <fredriknystroms@gmail.com>
2024-02-21 15:36:27 +0100

    phonebook step4

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '+1 123 123'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -16,7 +18,8 @@ const App = () => {
     } else {
       const newPerson = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(newPerson))
     }
@@ -33,8 +36,25 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
+
+  const filteredPersons = newSearch.length === 0 ? persons : persons.filter(person => person.name.toLowerCase().startsWith(newSearch.toLowerCase()))
+  
   return (
     <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with: 
+          <input 
+            value={newSearch}
+            onChange={handleSearch}
+          />
+        </div>
+      </form>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
@@ -56,8 +76,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
-        <p key={person.name}>{person.name} {person.number}</p>
+      {filteredPersons.map(person =>
+        <p key={person.id}>{person.name} {person.number}</p>
       )}
       
     </div>
