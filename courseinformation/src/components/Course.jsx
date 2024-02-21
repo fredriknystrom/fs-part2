7bf8f4f49f7de925bd2a99029b18ac59365bc035
fredriknystrom <fredriknystroms@gmail.com>
2024-02-21 13:08:59 +0100

    courseinformation step7, step8

--- courseinformation/src/components/Course.jsx
+++ courseinformation/src/components/Course.jsx
@@ -14,18 +14,28 @@ const Header = (props) => {
   }
   
   const Content = (props) => {
+
+    const total = props.parts.reduce((s, p) => {
+        return(
+            s + p.exercises
+        )
+    }, 0)
+
     return (
       <div>
         {props.parts.map(part => 
           <Part key={part.id} name={part.name} exercises={part.exercises} />
         )}
+        <p>total of {total} exercises</p>
       </div>
+
     )
   }
 
 const Course = (props) => {
     console.log(props)
     const {course } = props
+
     return(
         <div>
             <Header course={course.name}/>
